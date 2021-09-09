306ac46dbe7d43061d0fbd113aede89f89fe6ed6
MyungJewon <jj4234@naver.com>
2021-09-09 11:19:51 +0900

    update 210909

---
+++ VRForest/Assets/Script/c_wind.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c_wind : MonoBehaviour
+{
+    public AudioSource audio1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider coll){
+        if(coll.gameObject.CompareTag("Player")){
+            audio1.Play();
+		}
+	}
+}
